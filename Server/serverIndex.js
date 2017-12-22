0d1fba8bdbdd0735689da4b4222a6599fb588186
Edwin Chu <edwinychu@gmail.com>
2017-12-21 16:16:57 -0800

    server file update

--- Server/serverIndex.js
+++ Server/serverIndex.js
@@ -1,3 +1,4 @@
+// const newrelic = require('newrelic');
 const express = require('express');
 const dotenv = require('dotenv');
 const db = require('../Database/dbIndex.js');
@@ -13,36 +14,34 @@ const PORT = process.env.PORT || 8080;
 
 app.use(bodyparser.json());
 
-// for client polling
-app.post('/polling', (req, res) => {
-  const { rideId } = req.query;
-  db.getRideInfo(rideId).then((ride) => {
+app.post('/polling', async (req, res) => {
+  const ride = await db.getRideInfo(req.body.rideId);
+  if (ride !== undefined) {
     if (ride.wait_est !== null) {
-      const waitEst = { wait_est: ride.wait_est };
-      res.json(waitEst);
+      res.json({ wait_est: ride.wait_est });
     } else {
       res.send('No driver matches yet.');
     }
-  });
+  } else {
+    res.send('No driver matches yet.');
+  }
 });
 
-// initial bookings from client
-app.post('/bookings', async (req, res) => {
-  // deployed version
-  const riderInfo = req.body; // rider_id, start_loc, end_loc
-  const rideId = unique();
-  const startLoc = `POINT(${riderInfo.start_loc})`;
+app.post('/bookings', (req, res) => {
+  const startLoc = `POINT(${req.body.start_loc})`;
 
-  // generate timestamp with unix date at that moment
-  riderInfo.ride_id = rideId;
-  riderInfo.timestamp = Math.round(Date.now() / 1000);
-  db.saveUnmatchedRideInfo(riderInfo);
-  // const inventoryRideInfo = {
-  //   start_loc: startLoc,
-  //   ride_id: rideId,
-  // };
-  // await axios.post('http://localhost:8080/new_ride', inventoryRideInfo).catch((err) => {});
-  res.end();
+  req.body.ride_id = unique();
+  req.body.timestamp = Math.round(Date.now() / 1000);
+
+  db.saveUnmatchedRideInfo(req.body);
+
+  const inventoryRideInfo = {
+    start_loc: startLoc,
+    ride_id: req.body.ride_id,
+  };
+  // axios.post('http://localhost:8080/new_ride', inventoryRideInfo).catch((err) => {
+  // });
+  res.send();
   // res.send(rideId);
 });
 
@@ -50,47 +49,33 @@ app.post('/new_ride', (req, res) => {
   res.end();
 });
 
-// getting updated ride_id's from Dispatch service
-app.post('/updated', async (req, res) => {
-  // deployed version
-  const updatedRideInfo = req.body; // contains ride_id, driver_id, wait_est
-  const { ride_id } = req.body;
-  // update unmatched ride_id in database and store unmatched ride_id in cache
-  db.updateUnmatchedRideInfo(ride_id, updatedRideInfo);
+app.post('/updated', (req, res) => {
+  // contains ride_id, driver_id, wait_est
+  db.updateUnmatchedRideInfo(req.body.ride_id, req.body);
+  res.end();
 });
 
-// client sends either cancelled or completed status
-app.post('/cancelled', (req, res) => {
-  const { cancelledStatus } = req.body;
-  const { ride_id } = req.body;
-
-  // Cancelled Status should be determined by client
+app.post('/cancelled', async (req, res) => {
   // const cancelledStatus = Math.floor(Math.random() * (2 - 0));
-  // this service calculated cancellation time
+
   let cancellationTime;
-  if (cancelledStatus) {
-    if (waitEst > 5) {
+  if (req.body.cancelled) {
+    if (req.body.wait_est > 5) {
       cancellationTime = Math.floor(Math.random() * (3 - 1) + 1);
     } else {
-      cancellationTime = Math.floor(Math.random() * (waitEst - 1) + 1);
+      cancellationTime = Math.floor(Math.random() * (req.body.wait_est - 1) + 1);
     }
   } else {
-    cancellationTime = 0;
+    cancellationTime = null;
   }
 
-  // retrieve ride_id from db
-  db.getRideInfo(ride_id).then((ride) => {
-    const updatedRideInfo = ride;
-    // attach cancellation Time and cancelled status onto data
-    updatedRideInfo.cancellationTime = cancellationTime;
-    updatedRideInfo.cancelledStatus = cancelledStatus;
-    // only in deployed version
-    axios.post('http://localhost:8080/message_bus', updatedRideInfo);
-  });
-});
+  const ride = await db.getRideInfo(req.body.ride_id);
 
-app.post('/message_bus', (req, res) => {
-  console.log('finished sending to message bus');
+  ride.cancellationTime = cancellationTime;
+  ride.cancelledStatus = req.body.cancelled;
+
+  // axios.post('http://localhost:8080/message_bus', ride);
+
   res.end();
 });
 
@@ -104,4 +89,4 @@ if (!module.parent) {
   });
 }
 
-module.exports = app;
+module.exports.app = app;
